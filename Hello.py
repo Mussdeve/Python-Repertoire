d7c8f93292a77614a9b7430bf71016858699e889
Mussdeve <96483574+Mussdeve@users.noreply.github.com>
2022-01-10 13:49:38 +0100

    Create

---
+++ Hello.py
@@ -0,0 +1,27 @@
+#msg="Hello world
+#print(msg)
+
+a=7
+print(a+3)
+b=3
+print(a+b) 
+# afficher le résultat des valeurs respectives des variables a et b 
+
+# print(a)
+# a = b + 1
+# print(a)
+# print(a+b)
+
+# a += b #a = a + b
+print(a)
+
+a,b =11, 4
+c=a//b
+d=a%b
+print('la division de '+ str(a)+ ' par ' + str(b) + ' est égale à ' +str(c)+'. Il reste '+ str(d))
+print(d)
+
+chaine = 'Et voilà du texte'
+chaine1 = 'Nous l\'avons'
+chaine2 = "\"réparée\""
+print(chaine)
