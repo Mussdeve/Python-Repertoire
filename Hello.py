9e49587a7eebfdebce57f7e993f4245cf542fcd6
Mussdeve <96483574+Mussdeve@users.noreply.github.com>
2022-01-10 15:39:12 +0100

    Summary

--- Hello.py
+++ Hello.py
@@ -24,4 +24,31 @@ print(d)
 chaine = 'Et voilà du texte'
 chaine1 = 'Nous l\'avons'
 chaine2 = "\"réparée\""
-print(chaine)
+print(chaine1 + " " + chaine2)
+
+reponse = input () #une ligne vide apparait et attend que l'utilisateur entre une information
+
+age = int(input ("Quel est vôtre âge ?"))
+print("Vous avez " + str(age)+ " ans")
+
+if age > 16 and age < 100 :
+    print("Vous avez plus de 16 ans")
+elif age > 100 or age < 0:
+    print("Tu te moquerais pas de moi ?")
+else :
+    print("Tu es encore un peu jeune")
+
+a=3
+print(type(a))
+
+b=8.2
+print(type(b))
+
+chaine="Au revoir"
+print(type(chaine))
+vrai= False 
+print(type(vrai))
+
+print(7 > 5 > 2)
+print(b<a)
+
---
+++ Test2.py
@@ -0,0 +1,5 @@
+a = input("a:")
+b = input("b:")
+while b != 0:
+    a, b = b, a%b
+print(a)
