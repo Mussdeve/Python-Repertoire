129dc88c844a7c65745a7617d624f4f8b0da8565
Mussdeve <96483574+Mussdeve@users.noreply.github.com>
2022-01-17 09:59:03 +0100

    upd

--- Exo123Maison.py
+++ Exo123Maison.py
@@ -1,22 +1,23 @@
 #Exercice n°1
-"""
+
 a = int(input('Entrez la première valeur : '))
 b = int(input('Entrez la deuxième valeur : '))
 c = int(input('Entrez la troisième valeur : '))
 
-if (0 <a and b and c< 40000):
+if 0 <a and b and c< 40000:
     print((a+b+c)/3)
-"""
+
 # Exercice n°2
-"""
+
 a = int(input("Entrez une valeur : "))
 
 if a >= 0:
     print('Ce nombre est positif ou nul')
 elif a<0:
     print('Ce nombre est négatif')
-"""
+
 # Exercice n°3
+
 a = int(input('Ecrivez une valeur : '))
 """ isAModulo3 = a % 3 == 0
 isAModulo5 = a % 5 == 0
